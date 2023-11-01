e86597a20a5b6fd462cfb51b6b06d4fb08c27bdc
Sahithyan <sahithyan2701@gmail.com>
2023-11-01 20:55:36 +0530

    integrate backend in CreateManager.js

--- frontend/src/Components/CreateManager.js
+++ frontend/src/Components/CreateManager.js
@@ -1,18 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "./Parts.css";
-import { backend } from "../utilities";;
+import { backend } from "../utilities";
+import { useNavigate } from "react-router-dom";
 
 export function CreateManager() {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		branch: "none",
+		nicNumber: "",
+		phoneNumber: "",
+		address: "",
+		emailAddress: "",
+		dateOfBirth: "",
+		gender: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const [error, setError] = useState("");
 	const [branches, setBranches] = useState([]);
-	
+
+	const navigate = useNavigate();
+
 	useEffect(() => {
-		backend("/branches").then(data => {
+		backend("/branches").then((data) => {
 			if (typeof data == "string") {
-				console.error(data);return;
+				console.error(data);
+				return;
 			}
 			setBranches(data);
-		})
+		});
 	}, []);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
 
 	return (
 		<div className="Auth-form-container-big">
@@ -31,6 +58,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter first name"
+								name="firstName"
+								value={formData.firstName}
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -44,6 +74,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter last name"
+								name="lastName"
+								value={formData.lastName}
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -53,10 +86,19 @@ export function CreateManager() {
 							<label for="branch">Branch</label>
 						</div>
 						<div className="col-75">
-							<select id="branch" name="branch">
+							<select
+								id="branch"
+								name="branch"
+								value={formData.branch}
+								onChange={handleChange}
+							>
 								<option value="none">Choose an option</option>
 								{branches.map((branch) => {
-									return <option value={branch.branch_id} key={branch.branch_id}>{branch.branch_name}</option>
+									return (
+										<option value={branch.branch_id} key={branch.branch_id}>
+											{branch.branch_name}
+										</option>
+									);
 								})}
 							</select>
 						</div>
@@ -71,6 +113,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter NIC number"
+								name="nicNumber"
+								onChange={handleChange}
+								value={formData.nicNumber}
 							/>
 						</div>
 					</div>
@@ -84,6 +129,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter telephone number"
+								name="phoneNumber"
+								value={formData.phoneNumber}
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -97,6 +145,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter address"
+								value={formData.address}
+								name="address"
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -111,6 +162,9 @@ export function CreateManager() {
 								className="form-control mt-1"
 								placeholder="Enter email address"
 								size="48"
+								name="emailAddress"
+								onChange={handleChange}
+								value={formData.emailAddress}
 							/>
 						</div>
 					</div>
@@ -120,7 +174,13 @@ export function CreateManager() {
 							<label>Date of Birth</label>{" "}
 						</div>
 						<div className="col-75">
-							<input type="date" className="form-control mt-1" />
+							<input
+								type="date"
+								className="form-control mt-1"
+								name="dateOfBirth"
+								onChange={handleChange}
+								value={formData.dateOfBirth}
+							/>
 						</div>
 					</div>
 
@@ -129,9 +189,23 @@ export function CreateManager() {
 							<label>Gender</label>{" "}
 						</div>
 						<div className="col-75">
-							<input type="radio" id="male" name="gender" value="Male" />
+							<input
+								type="radio"
+								id="male"
+								name="gender"
+								value="Male"
+								onChange={handleChange}
+								checked={formData.gender == "Male"}
+							/>
 							<label for="male">Male</label>
-							<input type="radio" id="female" name="gender" value="Female" />
+							<input
+								type="radio"
+								id="female"
+								name="gender"
+								value="Female"
+								onChange={handleChange}
+								checked={formData.gender == "Female"}
+							/>
 							<label for="female">Female</label>
 							<br />
 						</div>
@@ -146,6 +220,9 @@ export function CreateManager() {
 								type="text"
 								className="form-control mt-1"
 								placeholder="Enter username"
+								name="username"
+								value={formData.username}
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -160,6 +237,9 @@ export function CreateManager() {
 								className="form-control mt-1"
 								placeholder="Enter password"
 								size="48"
+								value={formData.password}
+								name="password"
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
@@ -174,13 +254,35 @@ export function CreateManager() {
 								className="form-control mt-1"
 								placeholder="Confirm password"
 								size="48"
+								name="confirmPassword"
+								value={formData.confirmPassword}
+								onChange={handleChange}
 							/>
 						</div>
 					</div>
 
 					<br />
 					<div className="row">
-						<input type="submit" value="Submit" />
+						<input
+							type="submit"
+							value="Submit"
+							onClick={(event) => {
+								event.preventDefault();
+								backend("/create-manager", {
+									method: "POST",
+									body: formData,
+								}).then((data) => {
+									if (typeof data == "string") {
+										console.error(data);
+										setError(data);
+										return;
+									}
+									setTimeout(() => {
+										navigate(-1);
+									}, 1000);
+								});
+							}}
+						/>
 					</div>
 
 					<br />
@@ -196,20 +298,4 @@ export function CreateManager() {
 			</form>
 		</div>
 	);
-}	
-
-    //                         <div className="row">
-    //                             <div className="col-25">
-    //                                 <label for="branch">Branch</label>
-    //                             </div>
-    //                             <div className="col-75">
-    //                                 <select id="branch" name="branch">
-    //                                     <option value="none">Choose an option</option>
-    //                                     <option value="anseboileau">Anse Boileau</option>
-    //                                     <option value="glacis">Glacis</option>
-    //                                     <option value="plaisance">Plaisance</option>
-    //                                     <option value="Takamaka">Takamaka</option>
-    //                                     <option value="victoria">Victoria</option>
-    //                                 </select>
-    //                             </div>
-    //                         </div>
+}
