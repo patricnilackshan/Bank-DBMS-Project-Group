4ac94a801ba3fe13fcaa61f2c18035a5f04370ac
Sahithyan <sahithyan2701@gmail.com>
2023-11-01 11:13:16 +0530

    use jsx in Admin.js

--- frontend/src/Components/Admin.js
+++ frontend/src/Components/Admin.js
@@ -2,27 +2,69 @@ import React from 'react';
 import './Parts.css';
 
 export function Admin() {
-    const htmlContent = `
-        <!DOCTYPE html>
-        <html>
-            <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            </head>
-            <body>
-                <div class="Auth-form-container">
-                    <form class="Auth-form" action="/Admin">
-                        <div class="Auth-form-content">
-                            <h3 class="Auth-form-title" align="center">Welcome Back!</h3>                   
-                            <button class="button" style="vertical-align:middle" formAction="/CreateManager"><span>Create a Manager Account</span></button><br>
-                            <button class="button" style="vertical-align:middle" formAction="/CreateEmployee"><span>Create an Employee Account</span></button><br>
-                            <button class="button" style="vertical-align:middle" formAction="/ReportHistory"><span>View Report Generation History</span></button><br>
-                            <button class="button" style="vertical-align:middle" formAction="/"><span>Log Out</span></button>                          
-                        </div>
-                    </form>
-                </div>
-            </body>
-        </html>
-    `;
+	// const htmlContent = `
+	//     <!DOCTYPE html>
+	//     <html>
+	//         <head>
+	//         <meta name="viewport" content="width=device-width, initial-scale=1">
+	//         </head>
+	//         <body>
+	//             <div class="Auth-form-container">
+	//                 <form class="Auth-form" action="/Admin">
+	//                     <div class="Auth-form-content">
+	//                         <h3 class="Auth-form-title" align="center">Welcome Back!</h3>
+	//                         <button class="button" style="vertical-align:middle" formAction="/CreateManager"><span>Create a Manager Account</span></button><br>
+	//                         <button class="button" style="vertical-align:middle" formAction="/CreateEmployee"><span>Create an Employee Account</span></button><br>
+	//                         <button class="button" style="vertical-align:middle" formAction="/ReportHistory"><span>View Report Generation History</span></button><br>
+	//                         <button class="button" style="vertical-align:middle" formAction="/"><span>Log Out</span></button>
+	//                     </div>
+	//                 </form>
+	//             </div>
+	//         </body>
+	//     </html>
+	// `;
 
-    return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+	// return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+	return (
+		<div class="Auth-form-container">
+			<form class="Auth-form" action="/Admin">
+				<div class="Auth-form-content">
+					<h3 class="Auth-form-title" align="center">
+						Welcome Back!
+					</h3>
+					<button
+						class="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/CreateManager"
+					>
+						<span>Create a Manager Account</span>
+					</button>
+					<br />
+					<button
+						class="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/CreateEmployee"
+					>
+						<span>Create an Employee Account</span>
+					</button>
+					<br />
+					<button
+						class="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/ReportHistory"
+					>
+						<span>View Report Generation History</span>
+					</button>
+					<br />
+					<button
+						class="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/"
+					>
+						<span>Log Out</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
