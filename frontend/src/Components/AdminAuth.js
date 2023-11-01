9e52878daa29be36dd176afc67c9bda00d519d94
Sahithyan <sahithyan2701@gmail.com>
2023-11-01 11:07:03 +0530

    integrate /login in AdminAuth.js

--- frontend/src/Components/AdminAuth.js
+++ frontend/src/Components/AdminAuth.js
@@ -1,46 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { backend } from "../utilities";
+import { useNavigate } from "react-router-dom";
 
 export default function (props) {
-  return (
-    <div className="Auth-form-container">
-      <form className="Auth-form-small">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Welcome to A!</h3>
+	const [username, setUsername] = useState("admin");
+	const [password, setPassword] = useState("password");
+	const [loginError, setLoginError] = useState("");
 
-          <div className="form-group mt-3">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control mt-1"
-              placeholder="Enter username"
-            />
-          </div>
+	const navigate = useNavigate();
 
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
+	return (
+		<div className="Auth-form-container">
+			<form className="Auth-form-small">
+				<div className="Auth-form-content">
+					<h3 className="Auth-form-title">Welcome to A!</h3>
 
-          <div className="d-grid gap-2 mt-3">
-            <button formAction="/Admin" type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
+					<div className="form-group mt-3">
+						<label>Username</label>
+						<input
+							onChange={(event) => {
+								setUsername(event.target.value);
+							}}
+							type="text"
+							className="form-control mt-1"
+							placeholder="Enter username"
+							value={username}
+						/>
+					</div>
 
-          <div className="d-grid gap-2 mt-3">
-            <button className="backbutton-small" style={{ verticalAlign: "middle" }} formAction="/" type="submit"><span>Back </span></button>
-            </div>
+					<div className="form-group mt-3">
+						<label>Password</label>
+						<input
+							value={password}
+							type="password"
+							className="form-control mt-1"
+							onChange={(event) => {
+								setPassword(event.target.value);
+							}}
+							placeholder="Enter password"
+						/>
+					</div>
+					{loginError == "" ? null : <div>{loginError}</div>}
 
-          <p className="forgot-password text-mid mt-3" align="center">
-             <a href="/PasswordResetAdmin"  style={{ color: "black" }}>Forgot password?</a>
-          </p>
+					<div className="d-grid gap-2 mt-3">
+						<button
+							type="submit"
+							className="btn btn-primary"
+							onClick={(event) => {
+								event.preventDefault();
+								backend("/login", {
+									method: "POST",
+									body: {
+										username,
+										password,
+										type: "Admin",
+									},
+								}).then((data) => {
+									if (typeof data == "string") {
+										setLoginError(data);
+										return;
+									}
+									navigate("/Admin");
+								});
+							}}
+						>
+							Submit
+						</button>
+					</div>
 
-        </div>
-      </form>
-    </div>
-  )
+					<div className="d-grid gap-2 mt-3">
+						<button
+							className="backbutton-small"
+							style={{ verticalAlign: "middle" }}
+							formAction="/"
+							type="submit"
+						>
+							<span>Back </span>
+						</button>
+					</div>
+
+					<p className="forgot-password text-mid mt-3" align="center">
+						<a href="/PasswordResetAdmin" style={{ color: "black" }}>
+							Forgot password?
+						</a>
+					</p>
+				</div>
+			</form>
+		</div>
+	);
 }
