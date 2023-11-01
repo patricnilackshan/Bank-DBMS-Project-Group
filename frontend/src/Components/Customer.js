463ec71191eb47bd64e37f6e3606e498ccdf0d23
Sahithyan <sahithyan2701@gmail.com>
2023-11-01 20:57:42 +0530

    use jsx in Customer.js

--- frontend/src/Components/Customer.js
+++ frontend/src/Components/Customer.js
@@ -1,49 +1,100 @@
-import React from 'react';
-import './Parts.css';
+import React from "react";
+import "./Parts.css";
 
 export function Customer() {
-  return (
-    <div className="Auth-form-container-big">
-      <form className="Auth-form" action="/Customer">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title" align="center">Welcome Back!</h3>
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/ViewCustomer">
-            <span>Get Profile Details</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/ViewBalance">
-            <span>Get Account Details</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/Transaction">
-            <span>Do a Transaction</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/ApplyLoan">
-            <span>Apply for an Online Loan</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/CreateFD">
-            <span>Start a Fixed Deposit</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/CheckInstallments">
-            <span>Check Loan Installments</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/PayInstallment">
-            <span>Pay Loan Installments</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/Statement">
-            <span>Get Bank Statement</span>
-          </button>
-          <br />
-          <button className="button" style={{ verticalAlign: 'middle' }} formAction="/">
-            <span>Log Out</span>
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className="Auth-form-container-big">
+			<form className="Auth-form" action="/Customer">
+				<div className="Auth-form-content">
+					<h3 className="Auth-form-title" align="center">
+						Welcome Back!
+					</h3>
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/ViewCustomer"
+					>
+						<span>Get Profile Details </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/ViewBalance"
+					>
+						<span>Get Account Details </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/Transaction"
+					>
+						<span>Do a Transaction </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/ApplyLoan"
+					>
+						<span>Apply for an Online Loan </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/CreateFD"
+					>
+						<span>Start a Fixed Deposit </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/CheckInstallments"
+					>
+						<span>Check Loan Installments </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/PayInstallment"
+					>
+						<span>Pay Loan Installments </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/Statement"
+					>
+						<span>Get Bank Statement </span>
+					</button>
+					<br />
+					<button
+						className="button"
+						style={{ verticalAlign: "middle" }}
+						formAction="/"
+					>
+						<span>Log Out</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+
+	const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+            </body>
+        </html>
+    `;
+
+	return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
